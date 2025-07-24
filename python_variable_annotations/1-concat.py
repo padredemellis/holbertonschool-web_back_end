83c9f389dd29c4c2f8abf80d7f17ed80c39dbcb5
root <root@emadev.>
2025-07-24 12:26:12 -0300

    1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Este módulo contiene una función para concatenar dos cadenas.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenar dos cadenas.
+    """
+    return str1 + str2
