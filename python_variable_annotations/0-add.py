890be31f9734df4e63e7174e49c185f8b629ead7
root <root@emadev.>
2025-07-24 12:19:33 -0300

    0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
+'''
+Este módulo contiene una función para sumar dos números flotantes.
+'''
 def add(a: float, b: float) -> float:
+    """
+    Suma dos números flotantes y devuelve su suma.
+
+    Args:
+        a (float): El primer número flotante.
+        b (float): El segundo número flotante.
+
+    Returns:
+        float: La suma de a y b.
+    """
     return a + b
