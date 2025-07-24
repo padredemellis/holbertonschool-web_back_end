f8890aab71b08589f2b3608463571a0729b6593d
root <root@emadev.>
2025-07-24 13:15:08 -0300

    5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -7,5 +7,5 @@ list input_list of floats as argument and returns their sum as a float.
 
 def sum_list(input_list: list[float]) -> float:
     """Recibe una lista y retorna su suma"""
-    resultado = sum(input_list)
+    resultado: float = sum(input_list)
     return resultado
