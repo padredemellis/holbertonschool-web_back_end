f568916c589b328057c156b1fae767c134824f8d
root <root@emadev.>
2025-07-26 18:53:01 -0300

    4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module that defines task_wait_n - executes task_wait_random n times concurrently
+"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Executes task_wait_random n times with max_delay.
+    Returns list of delays in ascending order (without using sort()).
+    """
+    delays = []
+    for task in asyncio.as_completed([task_wait_random(max_delay) for _ in range(n)]):
+        delay = await task
+        delays.append(delay)
+    return delays
