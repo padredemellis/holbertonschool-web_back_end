e0ae195eb9af2be9ac2dcaef35b422d3f7f9af04
root <root@emadev.>
2025-07-28 16:57:34 -0300

    0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,9 +10,10 @@ Use the random module.
 '''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     '''Genera un numero al azar 10 veces por cada segundo'''
     for _ in range(10):
         await asyncio.sleep(1)
