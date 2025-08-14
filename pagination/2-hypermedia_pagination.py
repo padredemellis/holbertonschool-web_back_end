55c95bac89dd54794c0855db80c59072cc7e7044
root <root@emadev.>
2025-08-14 10:26:35 -0300

    2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -59,7 +59,7 @@ class Server:
 
         start, end = index_range(page, page_size)
         dataset = self.dataset()
-        
+
         if start >= len(dataset):
             return []
         return dataset[start:end]
