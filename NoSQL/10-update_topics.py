675038fab5c73c7cddae0d0638985254661dcb87
root <root@emadev.>
2025-08-16 21:48:17 -0300

    10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-'''
-Python function that changes all topics of a school document based on the name
-'''
+"""
+Function that changes all topics of a school document based on the name
+"""
 
 
 def update_topics(mongo_collection, name, topics):
     """
-    Function that changes all topics of a school document based on the name
+    Changes all topics of a school document based on the name
+
     Args:
         mongo_collection: pymongo collection object
-        name:(string) will be the school name to update
-        topics: (list of strings) will be the list of topics approached in the school
-    Returns:
-        Document updated
+        name (string): school name to update
+        topics (list): list of topics approached in the school
     """
     mongo_collection.update_many(
-        {"name: name"},
+        {"name": name},
         {"$set": {"topics": topics}}
     )
