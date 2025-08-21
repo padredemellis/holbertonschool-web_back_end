c870ed916e75fd4a5936ad776b53c51559119214
root <root@emadev.>
2025-08-21 18:30:05 -0300

    0

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,40 @@
+/**
+ * ES6 Basics - Task 0
+ *
+ * This file demonstrates the use of `const` and `let` for variable declaration.
+ *
+ * @author Emanuel Romero
+ * @version 1.0.0
+ */
+
+/**
+ * Returns a constant string value.
+ *
+ * @returns {string} The string "I prefer const when I can."
+ */
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+/**
+ * Returns a fixed string value.
+ *
+ * @returns {string} The string " is okay"
+ */
+export function getLast() {
+  return ' is okay';
+}
+
+/**
+ * Returns a combination of a string and the result of another function.
+ * This function uses `let` to show that the variable's value can be reassigned.
+ *
+ * @returns {string} The combined string "But sometimes let is okay".
+ */
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
