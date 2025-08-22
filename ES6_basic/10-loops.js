5a03d571623b3a238dc619698dc276d2f8c449db
root <root@emadev.>
2025-08-22 10:29:33 -0300

    10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
-    array[idx] = appendString + value;
-  }
-  return array;
+export default function appendToEachArrayValue(array, appendString){
+    const result = [];
+    for (const value of array){
+        result.push(appendString + value);
+    }
+    return result;
 }
