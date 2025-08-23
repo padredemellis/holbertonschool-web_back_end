1aa9e8f2a989e180e65ea06c224a52b218fca42e
root <root@emadev.>
2025-08-23 19:54:54 -0300

    3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+export default class Currency {
+    constructor(code, name){
+        if (typeof code !== 'string'){
+            throw new TypeError('code must be a string');
+        }
+        if (typeof name !== 'string'){
+            throw new TypeError('Name must be a string');
+        }
+        this._code = code;
+        this._name = name;
+    };
+    get code (){
+        return this._code;
+    };
+    get name() {
+        return this._name;
+    };
+    set code (newCode){
+        if (typeof newCode !== 'string'){
+            throw new TypeError('code must be a string');
+        }
+        this._code = newCode;
+    };
+    set name (newName){
+        if (typeof newName !== 'string'){
+            throw new TypeError('name must be a string');
+        }
+        this._name = newName;
+    };
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    };
+}
