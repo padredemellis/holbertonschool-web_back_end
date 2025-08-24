a3311465dda9bf34e214c5135543e63f05305188
root <root@emadev.>
2025-08-23 21:18:48 -0300

    5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,27 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+
+    // This is the key logic for the "abstract" behavior
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+
+  // This method can be empty, as its only purpose is to be overridden
+  evacuationWarningMessage() {}
+}
