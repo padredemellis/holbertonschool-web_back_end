d30ab1083a4686c2ed355b977bdd8bccf2dc775d
root <root@emadev.>
2025-08-30 20:25:32 -0300

    4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,37 @@
-import getListStudents from "./0-get_list_students.js";
-import getStudentsByLocation from "./2-get_students_by_loc.js";
-
 export default function updateStudentGradeByCity(students, city, newGrades){
-    let filtrados = students.filter(student => student.location == city );
+    let filtrados = students.filter(function(student){
+        if (student.location === city){
+            return student;
+        }
+    });
+    
+    filtrados = filtrados.map(function(student){
 
+        let calificacionEncontrada = null;
+        
+        for (let i = 0; i < newGrades.length; i++) {
+            if (newGrades[i].studentId === student.id) {
+                calificacionEncontrada = newGrades[i];
+                break;
+            }
+        }
+        
+        if (calificacionEncontrada !== null) {
+            return {
+                id: student.id,
+                firstName: student.firstName,
+                location: student.location,
+                grade: calificacionEncontrada.grade
+            };
+        } else {
+            return {
+                id: student.id,
+                firstName: student.firstName,
+                location: student.location,
+                grade: 'N/A'
+            };
+        }
+    });
+    
+    return filtrados;
 }
--- ES6_data_manipulation/arrays.js
+++ ES6_data_manipulation/arrays.js
@@ -4,7 +4,26 @@ console.log(tecnologias)
 console.table(tecnologias)
 console.log(tecnologias[0])
 
-//Metodos de arreglos
+//Metodos de arreglos, agregar un elemento al arreglo
 const tec = ['HTML','CSS', 'JavaScript', 'React', 'Node.js']
-tec.push("Nest.js")
-console.table(tec)
+const nuevoArreglo = [...tec, "Nest.js"] //cree un nuevo arreglo copiando el anterior y le agregue una palabra al final
+const otroArreglo = ["Python", ...nuevoArreglo] // aqui le puse una palabra a otro arreglo pero al principio]
+console.table(otroArreglo)
+
+
+tec.shift() //elimina el primer elemento del arreglo
+
+const tec2 = tec.filter(function(tech){
+    if(tech !== "HTML") {
+        return tech
+    }
+})
+
+const tec3 = tec.map(function(tech){
+    if(tech === "Node.js") {
+        return 'Nest.js'
+    } else {
+        return tech
+    }
+})
+console.log(tec3)
