ee45b791e524ec448f29801c178bd39e3a71c7d8
root <root@emadev.>
2025-08-30 23:05:06 -0300

    9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList () {
+    let mapa = new Map();
+    mapa.set("Apples", 10);
+    mapa.set("Tomatoes",10);
+    mapa.set("Pasta", 1);
+    mapa.set("Rice",1);
+    mapa.set("Banana", 5);
+    return (mapa);
+}
