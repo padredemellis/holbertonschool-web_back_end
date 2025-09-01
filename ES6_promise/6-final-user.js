ca63e95518af4dc475b91037890287d8a5d7823a
root <root@emadev.>
2025-08-31 22:12:07 -0300

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,12 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName){
-    const sign = signUpUser(firstName, lastName); 
-    const upload = uploadPhoto(fileName);
-    const results = await Promise.allSettled([sign, upload]);
-    
-    return results.map(result => {
-        if (result.status === 'fulfilled') {
-            return { status: 'fulfilled', value: result.value };
-        } else {
-            return { status: 'rejected', value: result.reason };
-        }
-    });
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  })));
 }
