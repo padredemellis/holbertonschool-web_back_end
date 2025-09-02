d9a5e444d54ec0461056e6f22b1abb6a31e647cb
root <root@emadev.>
2025-09-01 23:23:57 -0300

    Endpoints

--- proyect_backEnd/app.py
+++ proyect_backEnd/app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return "Hello Flask"
 
+@app.route('/create-article')
+def create_article():
+    return 'Aqui podemos crear un articulo'
+
+@app.route('/article/<int:article_id>')
+def view_article(article_id):
+    return f'Estas viendo el articulo numero {article_id}'
+
+
 if __name__ == "__main__":
     app.run(debug = True)
