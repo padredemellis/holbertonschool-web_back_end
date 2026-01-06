b3f0e19628db22f02f9b6d826152e60e95ae32ad
padredemellis <10656@holbertonstudents.com>
2026-01-06 15:28:07 -0300

    0

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -8,8 +8,5 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
+  return `But sometimes let${getLast()}`;
 }
