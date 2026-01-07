1d69bfe3580510114017a052a3b295e47396e442
padredemellis <10656@holbertonstudents.com>
2026-01-07 12:35:24 -0300

    javaScripts-basics

---
+++ ES6_basic/javascript-basics-0.0.1/assets/js/objeto-literal.js
@@ -0,0 +1,27 @@
+// los objetos literales son como los diccionarios de python. Tienen la llave y el valor
+let personaje = {
+    nombre: 'Tony Stark',   
+    codeName: 'Ironman',
+    vivo: false,
+    edad:40,
+    coords: {
+        lat: 34.034,
+        lng: -118.70
+    },
+    trajes: ["Mark I",'Mark V', 'Hulkbuster'],
+    direccion: {
+        zip: '10880, 90265',
+        ubicacion: 'Malibu, California'
+    },
+};
+console.log(personaje);
+console.log(`Nombre: ${personaje.nombre}`);
+console.log(` Edad: ${personaje['edad']}`);
+console.log(`Longitud: ${personaje['coords']['lng']}`);
+console.log(`Latitud: ${personaje['coords']['lat']}`);
+console.log(`Cantidad de trajes: ${personaje.trajes.length}`);
+console.log(`Ultimo traje: ${personaje.trajes[personaje.trajes.length - 1]}`);
+
+const x = 'vivo';
+console.log(`Vivo: ${personaje[x]}`); 
+
