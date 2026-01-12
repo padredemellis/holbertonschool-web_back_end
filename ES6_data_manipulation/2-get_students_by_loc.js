ecb4a397df84b31d5cf9cdb48647086fb14b562d
padredemellis <10656@holbertonstudents.com>
2026-01-12 11:48:23 -0300

    ejercicio 2

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,3 @@
-import getListStudentIds from './1-get_list_student_ids.js';
-import getListStudents from './0-get_list_students.js';
-
 export default function getStudentsByLocation(listStudents, city) {
-  return listStudents.filter((student) => student.location == city);
+  return listStudents.filter((student) => student.location === city);
 }
