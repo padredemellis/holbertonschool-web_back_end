27df4d7516cacf8aa779ff9ba3391eec1bed1cd2
padredemellis <10656@holbertonstudents.com>
2026-01-12 17:38:30 -0300

    ejercicio 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,15 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
+
   const result = [];
+
   for (const a of set) {
-    if (a.startsWith(startString)) {
+    if (typeof a === 'string' && a.startsWith(startString)) {
       result.push(a.substring(startString.length));
     }
   }
+
   return result.join('-');
 }
