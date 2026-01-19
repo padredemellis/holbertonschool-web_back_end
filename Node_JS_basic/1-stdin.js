00e3f6ad1a522ac94909f9e4152f46f81f8ddcc3
padredemellis <10656@holbertonstudents.com>
2026-01-19 18:43:30 -0300

    1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-console.log('Welcome to Holberton School, what is your name?');
+console.log("Welcome to Holberton School, what is your name?");
 
-process.stdin.on('data', (data) => {
+process.stdin.on("data", (data) => {
   process.stdout.write(`Your name is: ${data}`);
   if (process.stdin.isTTY) {
     process.exit();
   }
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
 });
