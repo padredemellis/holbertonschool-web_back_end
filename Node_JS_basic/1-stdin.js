dd4c038a283f45000c15987020f97d9e44108273
padredemellis <10656@holbertonstudents.com>
2026-01-19 18:32:44 -0300

    1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+console.log("Welcome to Holberton School, what is your name?");
+
+process.stdin.setEncoding("utf-8");
+
+let nombre;
+
+process.stdin.on("data", (data) => {
+  nombre = data.trim();
+  console.log(`Your name is: ${nombre}`);
+  process.stdin.pause();
+});
+
+process.stdin.on("end", () => {
+  console.log("This important software is now closing");
+});
