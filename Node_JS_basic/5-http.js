3032afc4aa4806fd463c4ead8ada84240a3bb40e
padredemellis <10656@holbertonstudents.com>
2026-01-23 14:27:58 -0300

    6

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,26 +1,26 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
+    fs.readFile(path, "utf8", (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        reject(new Error("Cannot load the database"));
         return;
       }
 
       const lines = data
         .trim()
-        .split('\n')
-        .filter((line) => line.trim() !== '');
+        .split("\n")
+        .filter((line) => line.trim() !== "");
 
       if (lines.length <= 1) {
-        resolve('Number of students: 0');
+        resolve("Number of students: 0");
         return;
       }
 
-      const header = lines[0].split(',');
+      const header = lines[0].split(",");
       const fieldIndex = header.length - 1;
       const students = lines.slice(1);
 
@@ -28,7 +28,7 @@ function countStudents(path) {
 
       const fields = {};
       for (const line of students) {
-        const parts = line.split(',');
+        const parts = line.split(",");
         const firstName = parts[0];
         const field = parts[fieldIndex];
 
@@ -39,7 +39,9 @@ function countStudents(path) {
       }
 
       for (const [field, names] of Object.entries(fields)) {
-        output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+        output += `Number of students in ${field}: ${
+          names.length
+        }. List: ${names.join(", ")}\n`;
       }
 
       resolve(output.trim());
@@ -51,12 +53,12 @@ const app = http.createServer((req, res) => {
   const reqUrl = url.parse(req.url, true);
 
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader("Content-Type", "text/plain");
 
-  if (reqUrl.pathname === '/') {
-    res.end('Hello Holberton School!');
-  } else if (reqUrl.pathname === '/students') {
-    res.write('This is the list of our students\n');
+  if (reqUrl.pathname === "/") {
+    res.end("Hello Holberton School!");
+  } else if (reqUrl.pathname === "/students") {
+    res.write("This is the list of our students\n");
     countStudents(process.argv[2])
       .then((data) => {
         res.end(data);
@@ -67,7 +69,7 @@ const app = http.createServer((req, res) => {
       });
   } else {
     res.statusCode = 404;
-    res.end('Not Found');
+    res.end("Not Found");
   }
 });
 
